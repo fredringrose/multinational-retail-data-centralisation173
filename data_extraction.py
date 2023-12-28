463cba3a24af03937275750ccedf667a5508368c
Fred Ringrose <fred@Freds-MBP.lan>
2023-12-28 18:48:50 +0100

    Successful instance of engine in data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,32 +1,37 @@
+from database_utils import DatabaseConnector  # Import the DatabaseConnector class
 import pandas as pd
-from sqlalchemy import create_engine
-import yaml
-from database_utils import DatabaseConnector
 
 class DatabaseExtractor:
     def __init__(self, database_connector):
         self.database_connector = database_connector
 
+        
     def read_rds_table(self, table_name):
-        if table_name not in self.database_connector.tables:
-            print(f"Error: Table '{table_name}' not found in the database.")
-            return None
-
+        # Access the engine object from the DatabaseConnector instance
+        engine = self.database_connector.engine
         try:
             # Running SQL query and returning table in a DataFrame
             query = f"SELECT * FROM {table_name}"
-            df = pd.read_sql(query, self.database_connector.engine)
+            df = pd.read_sql(query, engine)
+            print(df)
             return df
         except Exception as e:
             print(f"Error reading table '{table_name}': {e}")
             return None
 
+
+        # Now you can use the 'engine' object for your extraction logic
+        # ...
+
 # Example usage:
-file_path = 'db_creds.yaml'
-database_connector = DatabaseConnector(file_path)
-database_extractor = DatabaseExtractor(database_connector)
+if __name__ == "__main__": # To su
+    file_path = 'db_creds.yaml'
+    database_connector = DatabaseConnector(file_path)
+    
+    # Assuming 'orders_table' is a valid table in the database
+    data_extractor = DatabaseExtractor(database_connector)
+    data_extractor.read_rds_table('orders_table')
 
-# Assuming 'orders_table' is a valid table in the database
-orders_data = database_extractor.read_rds_table('orders_table')
 
-# Use the 'orders_data' DataFrame as needed
+   
+
